602a19f00094e77492983368162eca8cfd4ac7a3
Alex Molnár <molnar.alex98@gmail.com>
2020-02-08 01:03:28 +0100

    * Added feature to fill in addtional data * That data is also included in the generation * Almost full document can be generated * Still needs to be handled manually: summary, and coaching interviews

--- netcast_parser.user.js
+++ netcast_parser.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Netcast Parser
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.2
 // @description  Userscript to help automate certain tasks about writing summary of games.
 // @author       alex.molnar
 // @match        http://netcasting.webpont.com/*
@@ -9,191 +9,238 @@
 // ==/UserScript==
 
 (function() {
-  'use strict';
+    'use strict';
 
-  console.log("Hello Netcast");
+    // global variables
 
-  // global variables
+    let originalHTML;
 
-  let originalHTML;
+    let expanded = false;
+    let expandables = [
+        'modalContent',
+        'expandField',
+        'extraField',
+        'oldStyleButton',
+        'redirectButton'
+    ];
 
-  let homeTeam;
-  let awayTeam;
-  let homeScore;
-  let awayScore;
-  let quarterScore;
-  let homeStarters;
-  let awayStarters;
-  let homeBench;
-  let awayBench;
+    let additionalData = {};
+    let dataCollectors = [
+        'group_input_field',
+        'round_input_field',
+        'location_input_field',
+        'fans_input_field',
+        'ref1_input_field',
+        'ref2_input_field',
+        'home_coach_input_field',
+        'away_coach_input_field'
+    ];
 
-
-  // functional/higher order functions
-
-  const an = (acc, elem) => acc + elem.attributes.rel.value;
-
-  function folder(acc, el) {
-      return acc + el;
-  }
-
-  function callBackHTML(){
-      originalHTML = document.body.innerHTML;
-      document.body.innerHTML = this.responseText;
-      document.getElementById("backgroundModal").style.animation = "fadeIn 0.4s 1";
-      document.getElementById("modalContent").style.animation = "fadeIn 1.0s 1";
-  }
-
-
-  // helper functions
-
-  function connectEventListeners(){
-      document.getElementById('redirectButton').addEventListener('click', redirectToNetcastServer);
-      document.getElementById('oldStyleButton').addEventListener('click', buildDownloadableTXT);
-      document.getElementById('close').addEventListener('click', function() { closeDialog(true); });
-  }
-
-  function converter(acc, item) {
-      let pointsFromThree = 3 * parseInt(item.querySelector(".stat3pts").querySelector(".TT3m").innerHTML);
-      let end = pointsFromThree == 0 ? "" : "/" + pointsFromThree;
-      return acc +
-          Array.from(item.querySelector(".statsquadraname").children)[0].innerHTML.split("&nbsp;").join(" ") +
-          " " +
-          Array.from(item.querySelector(".statpts").children)[0].innerHTML +
-          end +
-          ", ";
-  }
-
-  function isHomePlayer(home) {
-      return function(elem) {
-          return Array.from(elem.parentElement.parentElement.parentElement.classList).includes(home ? "hstat" : "astat");
-      }
-  }
-
-  function isBenchPlayer(elem) {
-      return !Array.from(elem.classList).includes("TTteam") && !Array.from(elem.classList).includes("TTtotal") && !Array.from(elem.classList).includes("starter");
-  }
-
-  function isStarter(elem) {
-      return Array.from(elem.classList).includes("starter");
-  }
+    let homeTeam;
+    let awayTeam;
+    let homeScore;
+    let awayScore;
+    let quarterScore;
+    let homeStarters;
+    let awayStarters;
+    let homeBench;
+    let awayBench;
 
 
-  // parser functions
+    // functional/higher order functions
 
-  function getTeam(home) {
-      return Array.from(
-          document.getElementsByClassName("boxheadertext")
-      ).filter(
-          elem => Array.from(
-              elem.parentNode.parentNode.classList
-          ).includes(home? "hstat" : "astat")
-      )[0].innerHTML;
-  }
+    const an = (acc, elem) => acc + elem.attributes.rel.value;
 
-  function getScore(home) {
-      return Array.from(
-          Array.from(
-              document.getElementsByClassName(home ? "hscore-numbers" : "ascore-numbers")
-          )[0].children
-      ).filter(
-          elem => !Array.from(elem.classList).includes("hidden")
-      ).reduce(an,"");
-  }
+    function folder(acc, el) {
+        return acc + el;
+    }
 
-  function buildQuarterScore() {
-      let quarterScoreBuilder = "";
-      for(let i = 1; i <= 4 ; i++) {
-          quarterScoreBuilder += document.getElementById("hp" + i).innerHTML;
-          quarterScoreBuilder += "-";
-          quarterScoreBuilder += document.getElementById("ap" + i).innerHTML;
-          quarterScoreBuilder += i == 4 ? "" : ", ";
-      }
-      return quarterScoreBuilder;
-  }
-
-  function getPlayers(home, starter) {
-      var rawStarters = Array.from(document.getElementsByClassName("odd")).concat(Array.from(document.getElementsByClassName("even"))).filter(starter ? isStarter : isBenchPlayer);
-      var starterNames = rawStarters.filter(isHomePlayer(home)).reduce(converter, "");
-      return starterNames.substr(0, starterNames.length - 2);
-  }
-
-  function parseData(){
-      homeTeam = getTeam(true);
-      awayTeam = getTeam(false);
-      homeScore = getScore(true);
-      awayScore = getScore(false);
-      quarterScore = buildQuarterScore();
-      homeStarters = getPlayers(true, true);
-      awayStarters = getPlayers(false, true);
-      homeBench = getPlayers(true, false);
-      awayBench = getPlayers(false, false);
-  }
+    function callBackHTML(){
+        originalHTML = document.body.innerHTML;
+        document.body.innerHTML = this.responseText;
+        document.getElementById("backgroundModal").style.animation = "fadeIn 0.4s 1";
+        document.getElementById("modalContent").style.animation = "fadeIn 1.0s 1";
+    }
 
 
-  //builder functions
+    // helper functions
 
-  function buildTXTFile() {
-      let header = "Nemzeti Egyetemi Kosárlabda Bajnokság, {csoport} csoport, {forduló}. forduló\n\n";
-      let middle = `${homeTeam} - ${awayTeam} ${homeScore}-${awayScore} (${quarterScore})\n\n` +
-          `{város}, {helyszín}, {nézőszám} néző. V.: {játékvezetők}.\n\n` +
-          `${homeTeam}: ${homeStarters}. Csere: ${homeBench}. Edző: {edzőnév}\n` +
-          `${awayTeam}: ${awayStarters}. Csere: ${awayBench}. Edző: {edzőnév}\n`;
-      let end = "\ntudosítás helye\n\nedzői értékelések helye";
-      return header + middle + end;
-  }
+    function connectEventListeners(){
+        document.getElementById('redirectButton').addEventListener('click', redirectToNetcastServer);
+        document.getElementById('oldStyleButton').addEventListener('click', buildDownloadableTXT);
+        document.getElementById('close').addEventListener('click', function() { closeDialog(true); });
+        document.getElementById('expandField').addEventListener('click', expandFields);
+    }
 
+    function converter(acc, item) {
+        let pointsFromThree = 3 * parseInt(item.querySelector(".stat3pts").querySelector(".TT3m").innerHTML);
+        let end = pointsFromThree == 0 ? "" : "/" + pointsFromThree;
+        return acc +
+            Array.from(item.querySelector(".statsquadraname").children)[0].innerHTML.split("&nbsp;").join(" ") +
+            " " +
+            Array.from(item.querySelector(".statpts").children)[0].innerHTML +
+            end +
+            ", ";
+    }
 
-  // display dialog
+    function isHomePlayer(home) {
+        return function(elem) {
+            return Array.from(elem.parentElement.parentElement.parentElement.classList).includes(home ? "hstat" : "astat");
+        }
+    }
 
-  function popDialog(){
-      var oReq = new XMLHttpRequest();
-      oReq.addEventListener("load", callBackHTML);
-      oReq.open("GET", "https://people.inf.elte.hu/qbbwwd/netcast/innerh.php");
-      oReq.send();
-  }
+    function isBenchPlayer(elem) {
+        return !Array.from(elem.classList).includes("TTteam") && !Array.from(elem.classList).includes("TTtotal") && !Array.from(elem.classList).includes("starter");
+    }
 
-  function closeDialog(close) {
-      console.log(close);
-      if (close) {
-          document.getElementById("modalContent").style.animation = "fadeOut 0.4s 1";
-          setTimeout(function() { closeDialog(false); }, 400);
-      } else {
-          document.body.innerHTML = originalHTML;
-      }
-  }
-
-
-  // downloading
-
-  function buildDownloadableTXT(){
-      let a = document.createElement('a');
-      let str = buildTXTFile();
-      a.href = "data:application/octet-stream,"+encodeURIComponent(str);
-      a.download = 'tudositas.txt';
-      a.click();
-  }
-
-  function redirectToNetcastServer() {
-      let address = "https://people.inf.elte.hu/qbbwwd/netcast/netcast_server.php";
-      address += `?hometeam=${homeTeam}&awayteam=${awayTeam}`;
-      address += `&homescore=${homeScore}&awayscore=${awayScore}`;
-      address += `&quarterscore=${quarterScore}`;
-      address += `&homestarters=${homeStarters}&awaystarters=${awayStarters}`;
-      address += `&homebench=${homeBench}&awaybench=${awayBench}`;
-      window.open(address, "_blank");
-  }
+    function isStarter(elem) {
+        return Array.from(elem.classList).includes("starter");
+    }
 
 
-  // main
+    // parser functions
 
-  function main() {
-      parseData();
-      popDialog();
-      setTimeout(connectEventListeners, 1000);
-  }
+    function getTeam(home) {
+        return Array.from(
+            document.getElementsByClassName("boxheadertext")
+        ).filter(
+            elem => Array.from(
+                elem.parentNode.parentNode.classList
+            ).includes(home? "hstat" : "astat")
+        )[0].innerHTML;
+    }
+
+    function getScore(home) {
+        return Array.from(
+            Array.from(
+                document.getElementsByClassName(home ? "hscore-numbers" : "ascore-numbers")
+            )[0].children
+        ).filter(
+            elem => !Array.from(elem.classList).includes("hidden")
+        ).reduce(an,"");
+    }
+
+    function buildQuarterScore() {
+        let quarterScoreBuilder = "";
+        for(let i = 1; i <= 4 ; i++) {
+            quarterScoreBuilder += document.getElementById("hp" + i).innerHTML;
+            quarterScoreBuilder += "-";
+            quarterScoreBuilder += document.getElementById("ap" + i).innerHTML;
+            quarterScoreBuilder += i == 4 ? "" : ", ";
+        }
+        return quarterScoreBuilder;
+    }
+
+    function getPlayers(home, starter) {
+        var rawStarters = Array.from(document.getElementsByClassName("odd")).concat(Array.from(document.getElementsByClassName("even"))).filter(starter ? isStarter : isBenchPlayer);
+        var starterNames = rawStarters.filter(isHomePlayer(home)).reduce(converter, "");
+        return starterNames.substr(0, starterNames.length - 2);
+    }
+
+    function parseData(){
+        homeTeam = getTeam(true);
+        awayTeam = getTeam(false);
+        homeScore = getScore(true);
+        awayScore = getScore(false);
+        quarterScore = buildQuarterScore();
+        homeStarters = getPlayers(true, true);
+        awayStarters = getPlayers(false, true);
+        homeBench = getPlayers(true, false);
+        awayBench = getPlayers(false, false);
+    }
 
 
-  // entrypoint delay
+    //builder functions
 
-  setTimeout(main, 6000);
+    function buildTXTFile() {
+        let header = "Nemzeti Egyetemi Kosárlabda Bajnokság, {csoport} csoport, {forduló}. forduló\n\n";
+        let middle = `${homeTeam} - ${awayTeam} ${homeScore}-${awayScore} (${quarterScore})\n\n` +
+            `{város}, {helyszín}, {nézőszám} néző. V.: {játékvezetők}.\n\n` +
+            `${homeTeam}: ${homeStarters}. Csere: ${homeBench}. Edző: {edzőnév}\n` +
+            `${awayTeam}: ${awayStarters}. Csere: ${awayBench}. Edző: {edzőnév}\n`;
+        let end = "\ntudosítás helye\n\nedzői értékelések helye";
+        return header + middle + end;
+    }
+
+    function buildAdditionalData() {
+        dataCollectors.forEach(function(item, index) {
+            let val = document.getElementById(item).value;
+            if(val.trim() != '') {
+                additionalData[item] = val;
+            }
+        });
+    }
+
+
+    // manipulating hmtl
+
+    function popDialog(){
+        var oReq = new XMLHttpRequest();
+        oReq.addEventListener("load", callBackHTML);
+        oReq.open("GET", "https://people.inf.elte.hu/qbbwwd/netcast/innerh.php");
+        oReq.send();
+    }
+
+    function closeDialog(close) {
+        console.log(close);
+        if (close) {
+            document.getElementById("modalContent").style.animation = "fadeOut 0.4s 1";
+            setTimeout(function() { closeDialog(false); }, 400);
+        } else {
+            document.body.innerHTML = originalHTML;
+        }
+    }
+
+    function expandFields() {
+        expandables.forEach(function(item, index) {
+            if(!expanded) {
+                document.getElementById(item).classList.add("expanded");
+            } else {
+                document.getElementById(item).classList.remove("expanded");
+            }
+        });
+        expanded = !expanded;
+    }
+
+    // downloading
+
+    function buildDownloadableTXT(){
+        let a = document.createElement('a');
+        let str = buildTXTFile();
+        a.href = "data:application/octet-stream,"+encodeURIComponent(str);
+        a.download = 'tudositas.txt';
+        a.click();
+    }
+
+    function redirectToNetcastServer() {
+        let address = "https://people.inf.elte.hu/qbbwwd/netcast/netcast_server.php";
+        address += `?hometeam=${homeTeam}&awayteam=${awayTeam}`;
+        address += `&homescore=${homeScore}&awayscore=${awayScore}`;
+        address += `&quarterscore=${quarterScore}`;
+        address += `&homestarters=${homeStarters}&awaystarters=${awayStarters}`;
+        address += `&homebench=${homeBench}&awaybench=${awayBench}`;
+        if(expanded) {
+            dataCollectors.forEach(function(item, index) {
+                let val = document.getElementById(item).value;
+                if(val.trim() != '') {
+                    address += `&${item}=${val}`;
+                }
+            });
+        }
+        window.open(address, "_blank");
+    }
+
+
+    // main
+
+    function main() {
+        parseData();
+        popDialog();
+        setTimeout(connectEventListeners, 1000);
+    }
+
+
+    // entrypoint delay
+
+    setTimeout(main, 6000);
 })();
--- netcast_server.php
+++ netcast_server.php
@@ -12,29 +12,31 @@ if (version_compare(PHP_VERSION,'5.1.0')>=0) {
 $TBS = new clsTinyButStrong; 
 $TBS->Plugin(TBS_INSTALL, OPENTBS_PLUGIN);
 
-$gendergroup = "{csoport}";
-$roundofplay = "{forduló}";
+
+$gendergroup = $_GET["group_input_field"] ?? "{csoport}";
+$roundofplay = $_GET["round_input_field"] ?? "{forduló}";
 $hometeam = $_GET["hometeam"];
 $awayteam = $_GET["awayteam"];
 $homescore = $_GET["homescore"];
 $awayscore = $_GET["awayscore"];
 $quarterscore = $_GET["quarterscore"];
-$city = "{város}";
-$court = "{helyszín}";
-$fansnum = "{nézőszám}";
-$referees = "{játékvezetők}";
+$city = $_GET["location_input_field"] ?? "{helyszín}";
+$court = "TODO: court";
+$fansnum = $_GET["fans_input_field"] ?? "{nézőszám}";
+$referees = isset($_GET["ref1_input_field"]) && $_GET["ref2_input_field"] ? ($_GET["ref1_input_field"] . ", " .$_GET["ref2_input_field"]) : "{játékvezetők}";
+// TODO: játékvezetők
 $homestarters = $_GET["homestarters"];
 $awaystarters = $_GET["awaystarters"];
 $homebench = $_GET["homebench"];
 $awaybench = $_GET["awaybench"];
-$homecoach = "{edzőnév}";
-$awaycoach = "{edzőnév}";
+$homecoach = $_GET["home_coach_input_field"] ?? "{edzőnév}";
+$awaycoach = $_GET["away_coach_input_field"] ?? "{edzőnév}";
 
-$template = 'tudositas_sablon.odt';
+$template = 'tudositas_sablon.docx';
 $TBS->LoadTemplate($template, OPENTBS_ALREADY_UTF8); 
 
 // $save_as = (isset($_POST['save_as']) && (trim($_POST['save_as'])!=='') && ($_SERVER['SERVER_NAME']=='localhost')) ? trim($_POST['save_as']) : '';
-$output_file_name = "tudositas_generated.odt";
+$output_file_name = "tudositas_generated.docx";
 $TBS->Show(OPENTBS_DOWNLOAD, $output_file_name);
 exit();
 ?>
